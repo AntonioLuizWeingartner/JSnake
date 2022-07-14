29406f2bbcc71f942e50a7d1056509fbe5da7583
AntonioLuizWeingartner <antonioluiz686@gmail.com>
2022-07-14 13:33:34 -0300

    ComponentArray implemented.

--- src/public/code/component.ts
+++ src/public/code/component.ts
@@ -1,12 +1,41 @@
 import { Entity } from "./entity";
 
 interface ComponentArray<T> {
-    add_component(ent : Entity) : number;
-    get_component(ent : Entity, index : number) : T;
-    delete_component(ent : Entity, index : number) : T;
+    add_component(ent : Entity) : T;
+    get_component(ent : Entity) : T;
+    delete_component(ent : Entity) : void;
 }
 
-/*
 class ComponentArrayImpl<T> implements ComponentArray<T> {
+
+    private t_constructor : new () => T;
+
+    private next_cp_id = 0;
+    private ent_to_cp : Map<Entity, T> = new Map();
+
+    constructor(t_constructor : new () => T) {
+        this.t_constructor = t_constructor;
+    }
+
+    add_component(ent : Entity) : T {
+        const new_cp_instance = new this.t_constructor();
+        this.ent_to_cp.set(ent, new_cp_instance);
+        return new_cp_instance;
+    }
+
+    get_component(ent : Entity) : T {
+        if (this.ent_to_cp.has(ent)) {
+            return this.ent_to_cp.get(ent) as T;
+        } else {
+            throw `Component not found. (Entity: ${ent})`;
+        }
+    }
+
+    delete_component(ent : Entity) : void {
+        if (this.ent_to_cp.has(ent)) {
+            this.ent_to_cp.delete(ent);
+        } else {
+            throw `Component not found. (Entity: ${ent})`;
+        }
+    }
 }
-*/
